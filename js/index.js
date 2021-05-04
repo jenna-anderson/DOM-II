2e2ae9797cfc57413d94f2cf1222c3930a30806c
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-05-04 13:50:21 -0600

    added event listener to nav links to change the font size

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+Array.from(navLinks).forEach(link => {
+    link.addEventListener('mouseover', enlarge);
+    link.addEventListener('mouseout', shrink);
+})
+
+function enlarge(event){
+    event.target.style.fontSize = '2rem';
+}
+
+function shrink(event){
+    event.target.style.fontSize = '1.6rem';
+}
