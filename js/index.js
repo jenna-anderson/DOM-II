76bc073a1327a85cfc01d2383b38f033fddfd1a1
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-05-04 14:27:13 -0600

    added event listeners to middle images to enlarge them when double clicked

--- js/index.js
+++ js/index.js
@@ -1,16 +1,46 @@
 // Your code goes here
 
+// nav events
+
 const navLinks = document.querySelectorAll('.nav-link');
 
 Array.from(navLinks).forEach(link => {
-    link.addEventListener('mouseover', enlarge);
-    link.addEventListener('mouseout', shrink);
+    link.addEventListener('mouseover', enlargeText);
+    link.addEventListener('mouseout', shrinkText);
 })
 
-function enlarge(event){
+function enlargeText(event){
     event.target.style.fontSize = '2rem';
 }
 
-function shrink(event){
+function shrinkText(event){
     event.target.style.fontSize = '1.6rem';
+}
+
+// middle content image events
+const midImages = document.querySelectorAll('.img-content');
+
+Array.from(midImages).forEach(image => {
+    image.addEventListener('dblclick', enlargeImage);
+    image.addEventListener('click', shrinkImage);
+})
+
+function enlargeImage(event){
+    event.target.style.transform = 'scale(1.3)';
+}
+
+function shrinkImage(event){
+    event.target.style.transform = 'scale(1)';
+}
+
+// button events
+
+const buttons = document.querySelectorAll('.btn');
+
+Array.from(buttons).forEach(button => {
+    button.addEventListener('click', popUp);
+})
+
+function popUp(){
+    alert('Sign up here!');
 }
